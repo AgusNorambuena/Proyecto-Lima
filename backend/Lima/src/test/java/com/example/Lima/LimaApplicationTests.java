ed0735c1955ef311f4c0fdb549e9455551a7994b
AgusNorambuena <agustinnoram@gmail.com>
2026-08-27 21:00:02 -0300

    feat: Creo el backend con java springboot y frontend con expo

---
+++ backend/Lima/src/test/java/com/example/Lima/LimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
