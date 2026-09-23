a08049c3ed449b6900fcf69358d1450c1b976b0b
AgusNorambuena <agustinnoram@gmail.com>
2026-09-23 01:57:26 -0300

    feat: creao el model de User con sus atributos y los campos para la BDD

---
+++ backend/Lima/src/main/java/com/escuela/Lima/model/User.java
@@ -0,0 +1,29 @@
+package com.escuela.Lima.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password; // Contraseña hasheada
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Password> passwords;
+
+}
